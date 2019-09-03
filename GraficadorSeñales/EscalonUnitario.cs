2153a5ff51d2976d912e537da90f6d36122e1fd2
Eduardo Antolín Jiménez <laloantolin@hotmail.com>
2019-09-03 11:00:11 -0700

    Add project files.

---
+++ GraficadorSeñales/EscalonUnitario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class EscalonUnitario
+    {
+        public List<Muestra> muestras { get; set; }
+
+        public EscalonUnitario()
+        {
+            muestras = new List<Muestra>();
+        }
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+            if (1 >= tiempo)
+            {
+                resultado = 1;
+            }
+            else
+            {
+                resultado = 0;
+            }
+            return resultado;
+        }
+    }
+}
